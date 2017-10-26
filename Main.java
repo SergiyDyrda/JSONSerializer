c2c4c0a18cd1ef81f2aa7a13ad3889cfc16f55e9
Sergiy Dyrda <vatapower12@hotmail.com>
2017-10-26 11:38:23 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,6 +5,6 @@ package com.netcracker.lab02;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Integer.class.equals(int.class));
+        
     }
 }
